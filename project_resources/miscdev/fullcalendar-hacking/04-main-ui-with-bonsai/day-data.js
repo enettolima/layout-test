716d7055df8940b8e62d5ab7c4b69d453dd0b044
Chad C. Davis <chad.davis@gmail.com>
2013-12-13 17:02:32 -0600

    Pulled block scheduling code over into the overview and began integrating the two.

---
+++ project_resources/miscdev/fullcalendar-hacking/04-main-ui-with-bonsai/day-data.js
@@ -0,0 +1,43 @@
+var sunday = [];
+sunday.push( { 'eid': '301LB', 'inouts': [ {'in':'09:00', 'out':'10:00'}, {'in':'13:00', 'out':'22:00'} ] });
+sunday.push( { 'eid': '301SR', 'inouts': [ {'in':'09:00', 'out':'10:30'}, {'in':'13:00', 'out':'20:15'} ] }); 
+
+var monday = [];
+monday.push( { 'eid': '301SM', 'inouts': [ {'in':'11:00', 'out':'14:00'}, {'in':'15:00', 'out':'19:00'} ] });
+monday.push( { 'eid': '301SM', 'inouts': [ {'in':'11:00', 'out':'14:00'}, {'in':'15:00', 'out':'19:00'} ] });
+monday.push( { 'eid': '301LB', 'inouts': [ {'in':'08:00', 'out':'12:00'}, {'in':'12:30', 'out':'17:00'} ] });
+monday.push( { 'eid': '301SR', 'inouts': [ {'in':'07:30', 'out':'11:00'}, {'in':'12:00', 'out':'18:00'} ] });
+
+var tuesday = [];
+tuesday.push( { 'eid': '301SM', 'inouts': [ {'in':'11:00', 'out':'14:00'}, {'in':'15:00', 'out':'19:00'} ] });
+tuesday.push( { 'eid': '301SM', 'inouts': [ {'in':'11:00', 'out':'14:00'}, {'in':'15:00', 'out':'19:00'} ] });
+tuesday.push( { 'eid': '301SR', 'inouts': [ {'in':'07:30', 'out':'11:00'}, {'in':'12:00', 'out':'18:00'} ] });
+
+var wednesday = [];
+wednesday.push( { 'eid': '301SM', 'inouts': [ {'in':'11:00', 'out':'14:00'}, {'in':'15:00', 'out':'19:00'} ] });
+wednesday.push( { 'eid': '301SR', 'inouts': [ {'in':'07:30', 'out':'11:00'}, {'in':'12:00', 'out':'18:00'} ] });
+wednesday.push( { 'eid': '301SM', 'inouts': [ {'in':'11:00', 'out':'14:00'}, {'in':'15:00', 'out':'19:00'} ] });
+wednesday.push( { 'eid': '301SR', 'inouts': [ {'in':'07:30', 'out':'11:00'}, {'in':'12:00', 'out':'18:00'} ] });
+wednesday.push( { 'eid': '301SR', 'inouts': [ {'in':'07:30', 'out':'11:00'}, {'in':'12:00', 'out':'18:00'} ] });
+
+var thursday = [];
+thursday.push( { 'eid': '301SM', 'inouts': [ {'in':'11:00', 'out':'14:00'}, {'in':'15:00', 'out':'19:00'} ] });
+thursday.push( { 'eid': '301SM', 'inouts': [ {'in':'11:00', 'out':'14:00'}, {'in':'15:00', 'out':'19:00'} ] });
+thursday.push( { 'eid': '301LB', 'inouts': [ {'in':'08:00', 'out':'12:30'}, {'in':'13:00', 'out':'22:00'} ] });
+thursday.push( { 'eid': '301SR', 'inouts': [ {'in':'07:30', 'out':'11:00'}, {'in':'12:00', 'out':'18:00'} ] });
+
+var friday = [];
+friday.push( { 'eid': '301LB', 'inouts': [ {'in':'08:00', 'out':'12:30'}, {'in':'13:00', 'out':'22:00'} ] });
+friday.push( { 'eid': '301SR', 'inouts': [ {'in':'07:30', 'out':'11:00'}, {'in':'12:00', 'out':'18:00'} ] });
+friday.push( { 'eid': '301SR', 'inouts': [ {'in':'07:30', 'out':'11:00'}, {'in':'12:00', 'out':'18:00'} ] });
+friday.push( { 'eid': '301SR', 'inouts': [ {'in':'07:30', 'out':'11:00'}, {'in':'12:00', 'out':'18:00'} ] });
+friday.push( { 'eid': '301SR', 'inouts': [ {'in':'07:30', 'out':'11:00'}, {'in':'12:00', 'out':'18:00'} ] });
+
+var saturday = [];
+saturday.push( { 'eid': '301SM', 'inouts': [ {'in':'11:00', 'out':'14:00'}, {'in':'15:00', 'out':'19:00'} ] });
+saturday.push( { 'eid': '301LB', 'inouts': [ {'in':'08:00', 'out':'12:30'}, {'in':'13:00', 'out':'22:00'} ] });
+saturday.push( { 'eid': '301SR', 'inouts': [ {'in':'07:30', 'out':'11:00'}, {'in':'12:00', 'out':'18:00'} ] });
+saturday.push( { 'eid': '301SR', 'inouts': [ {'in':'07:30', 'out':'11:00'}, {'in':'12:00', 'out':'18:00'} ] });
+saturday.push( { 'eid': '301SR', 'inouts': [ {'in':'07:30', 'out':'11:00'}, {'in':'12:00', 'out':'18:00'} ] });
+
+var days = [sunday, monday, tuesday, wednesday, thursday, friday, saturday]; 
---
+++ project_resources/miscdev/fullcalendar-hacking/04-main-ui-with-bonsai/overview.js
@@ -0,0 +1,6 @@
+
+$(document).ready(function(){
+    $('.day-button').click(function(){
+        window.location.href =  'day.html';
+    });
+});
