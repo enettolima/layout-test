5df414b39f782ac8adf915b1d838f199bfc6677a
Chad C. Davis <chad.davis@gmail.com>
2014-03-25 14:48:17 -0500

    Styles stuff

--- laravel/app/views/pages/dev/styles.blade.php
+++ laravel/app/views/pages/dev/styles.blade.php
@@ -8,12 +8,13 @@
     }
 </style>
 
-<div class="jumbotron"><h2>STYLEZ</h2></div>
-
+<div class="jumbotron"><h2>STYLE</h2></div>
 
 <strong class="text-primary">Hey there</strong>
 
-
 <p class="bg-danger pad15">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+
+<!-- Modal -->
 
 @stop
