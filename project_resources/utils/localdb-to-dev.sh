bc2e578498297fa046384c509c8276c306356714
Chad C. Davis <chad.davis@gmail.com>
2014-05-05 15:30:19 -0500

    Replication script

--- project_resources/utils/localdb-to-dev.sh
+++ project_resources/utils/localdb-to-dev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mysqldump -udbuser -pdbpass dbname | mysql -udbuser -p'this is literally the password for dbuser' ppdev_01 -hdev.ebtpassport.com
+mysqldump -v --skip-extended-insert -udbuser -pdbpass dbname | mysql -udbuser -p'this is literally the password for dbuser' ppdev_01 -hdev.ebtpassport.com
