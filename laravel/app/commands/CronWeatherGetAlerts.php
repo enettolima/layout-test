5ab39ca74175b6ee8b625f8e4351fffaa4c3d0e0
Chad C. Davis <chad.davis@gmail.com>
2014-08-05 12:02:13 -0500

    Added explicit all_clear = false to avoid nulls that make things confusing with query builder

--- laravel/app/commands/CronWeatherGetAlerts.php
+++ laravel/app/commands/CronWeatherGetAlerts.php
@@ -59,13 +59,12 @@ class CronWeatherGetAlerts extends Command {
 
                 foreach ($array['alerts'] as $alert) {
 
-                    //$message = $alert['message'];
-
                     $wa->type = $alert['type'];
                     $wa->description = $alert['description'];
                     $this->info("{$alert['type']} - {$alert['description']}");
                     $wa->alert_date = date("Y-m-d H:i:s", $alert['date_epoch']);
                     $wa->alert_expires = date("Y-m-d H:i:s", $alert['expires_epoch']);
+                    $wa->all_clear = false;
                     $wa->timezone = $alert['tz_short'];
 
                 }
@@ -84,46 +83,6 @@ class CronWeatherGetAlerts extends Command {
             sleep($sleepTime);
         }
 
-        /*
-        if (($handle = fopen("/home/chad/weather/Stores.csv", "r")) !== FALSE) {
-            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-
-                $city = $data[2];
-                $cityEnc = rawurlencode($city);
-                $state = rawurlencode($data[1]);
-
-                $contents = file_get_contents("http://api.wunderground.com/api/27603a980e3def63/alerts/q/$state/$cityEnc.json");
-
-                $array = json_decode($contents, true);
-
-                $storeNumber = $data[0];
-
-                if (array_key_exists('alerts', $array) && count($array['alerts']) > 0) {
-
-                    foreach ($array['alerts'] as $alert) {
-                        $alertDate = date("YmdHis", $alert['date_epoch']);
-                        $expireDate = date("YmdHis", $alert['expires_epoch']);
-                        $type = $alert['type'];
-                        $desc = $alert['description'];
-                        $tz = $alert['tz_short'];
-                        $message = $alert['message'];
-
-
-                        print "$storeNumber, $city, $state, $alertDate, $expireDate, $type, $desc, $tz\n";
-                    }
-                } else {
-                    $nowarnDate = date("YmdHis");
-                    print "$storeNumber, $city, $state, no warnings at $nowarnDate\n";
-                }
-
-                $onRow++;
-
-                sleep($sleepTime);
-            }
-
-            fclose($handle);
-        }
-        */
     }
 
     /**
