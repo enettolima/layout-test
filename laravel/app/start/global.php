b8b203d4df83b6ebca4e953db94024be3cfaa870
Chad C. Davis <chad.davis@gmail.com>
2014-09-24 11:44:15 -0500

    Turned off debugging by default (bad), and modified the fatal error reponse to be less obviously Laravel and provide a way back.

--- laravel/app/start/global.php
+++ laravel/app/start/global.php
@@ -50,6 +50,8 @@ Log::useFiles(storage_path().'/logs/laravel.log');
 App::error(function(Exception $exception, $code)
 {
 	Log::error($exception);
+
+    return 'Fatal Passport Error... go <a href="javascript:history.back();">back</a>?';
 });
 
 /*
