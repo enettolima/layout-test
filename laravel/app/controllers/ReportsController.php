4cc3300c565552e8522bbee000fc944fe7cc859b
Chad C. Davis <chad.davis@gmail.com>
2015-03-30 07:59:00 -0500

    Fixing reports direct access

--- laravel/app/controllers/ReportsController.php
+++ laravel/app/controllers/ReportsController.php
@@ -2,6 +2,12 @@
 
 class ReportsController extends BaseController
 {
+    /* Require Auth on Everything Here */
+    public function __construct()
+    {
+        $this->beforeFilter('auth', array());
+    }
+
     public function getIndex()
     {
         return View::make('pages.reports.index');
