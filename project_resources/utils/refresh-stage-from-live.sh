752fdcaeb259fe559988db9138b09fa4a5bf743c
WWW General <wwwgeneral@earthboundtrading.com>
2015-04-28 16:07:35 -0500

    cleanup

--- project_resources/utils/refresh-stage-from-live.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-mysqldump -v --skip-extended-insert -udbuser -p'this is literally the password for dbuser' passport_live -hlocalhost | mysql -udbuser -p'this is literally the password for dbuser' passport_stage
