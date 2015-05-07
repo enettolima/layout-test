f3676a2d32ec84f7b6aa58f1bd9d00194cd1d9aa
Chad C. Davis <chad.davis@gmail.com>
2015-05-07 13:35:02 -0500

    Fixing references to resources on CDN

--- laravel/app/views/includes/head.blade.php
+++ laravel/app/views/includes/head.blade.php
@@ -6,23 +6,21 @@
 
 <!-- Bootstrap core CSS -->
 
-<!--<link rel="stylesheet" href="/css/bootstrap.css">-->
-
 <!-- CSS -->
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="/css/bootstrap.min.css">
 
 <link rel="stylesheet" href="/css/local.css">
 
 <link rel="stylesheet" href="/css/ui-lightness/jquery-ui-1.10.3.custom.css" />
 
-<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="/css/font-awesome.min.css">
 
 <!-- Fuel UX CSS -->
 <!--<link href="/css/fuelux.css" rel="stylesheet">-->
 
 <!-- jQuery -->
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.js"></script>
 <script src="/js/jquery-git.js"></script>
+
 <script src="/js/jquery.cookie.js"></script>
 
 <script src="/js/main.js" type="text/javascript" charset="utf-8"></script>
