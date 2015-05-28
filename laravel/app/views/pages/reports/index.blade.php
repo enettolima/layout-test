c74db6362b3f32910597b7d828debe32ab926dd5
WWW General <wwwgeneral@earthboundtrading.com>
2015-05-28 11:17:43 -0500

    Added descriptions to reports

--- laravel/app/views/pages/reports/index.blade.php
+++ laravel/app/views/pages/reports/index.blade.php
@@ -10,15 +10,15 @@
 
         <dl>
             <dt><a href="reports/budget-sales-plan">Budget Sales Plan</a></dt>
-            <dd>The Budget Sales Plan report needs a description.</dd>
+            <dd>Use this report to get a daily summary of Sales vs Budget with percentage change ("over/under") for the entire month. The totals at the bottom of the report will provide you with the overall month numbers.</dd>
         </dl>
         <dl>
             <dt><a href="reports/all-star">All Star</a></dt>
-            <dd>The All-Star report also needs a description.</dd>
+            <dd>Employee Performance. Sales, Hours, Budget by each employee with Statistics ADS, UPT. Includes the Month Budget Summary vs Sales Summary and a quick check whether store made the goal or not.</dd>
         </dl>
         <dl>
             <dt><a href="reports/sales-plan-vs-sales">Sales Plan vs. Sales</a></dt>
-            <dd>If the other two reports have a description, it's only fair that the Sales Plan vs. Sales report does too.</dd>
+            <dd>Sales and Budget summary from the beginning of the year performed by your store. It will also provide a overall picture of the District and Region for easy comparison.</dd>
         </dl>
     </div>
 
