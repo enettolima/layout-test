e1ccfb8d69abd3edb6de0bf0c0e5d07fe5f765d6
Chad C. Davis <chad.davis@gmail.com>
2015-06-01 09:54:10 -0500

    Report Changes

--- laravel/public/js/reports/salesplanvssales/salesplanvssales.js
+++ laravel/public/js/reports/salesplanvssales/salesplanvssales.js
@@ -33,10 +33,10 @@ function loadReport(storeNumber)
                 "<td class='text-right'><strong>Month</strong></td>",
                 "<td class='text-right'><strong>Sales</strong></td>",
                 "<td class='text-right'><strong>Budget</strong></td>",
+                "<td class='text-right'><strong>Difference</strong></td>",
+                "<td class='text-right'><strong>Difference&nbsp;%</strong></td>",
                 "<td class='text-right'><strong>LY&nbsp;Sales&nbsp;MTD</strong></td>",
                 "<td class='text-right'><strong>Total&nbsp;Budget</strong></td>",
-                "<td class='text-right'><strong>Difference</strong></td>",
-                "<td class='text-right'><strong>%&nbsp;to&nbsp;Budget</strong></td>",
             "</tr>"
         );
 
@@ -66,32 +66,32 @@ function loadReport(storeNumber)
         {
             var tr = response.data[row];
 
-            tr.pSALES        = parseCurrency(tr.SALES);
-            tr.pLY_SALES_MTD = parseCurrency(tr.LY_SALES_MTD);
-            tr.pBUDGET       = parseCurrency(tr.BUDGET);
-            tr.pTOTAL_BUDGET = parseCurrency(tr.TOTAL_BUDGET);
-            tr.pSTR_AMT_Diff = parseCurrency(tr.STR_AMT_Diff);
-            tr.pSTR_PER_TO_BUDGET = parsePct(tr.STR_PER_TO_BUDGET);
+            tr.pSALES                = parseCurrency(tr.SALES);
+            tr.pLY_SALES_MTD         = parseCurrency(tr.LY_SALES_MTD);
+            tr.pBUDGET               = parseCurrency(tr.BUDGET);
+            tr.pTOTAL_BUDGET         = parseCurrency(tr.TOTAL_BUDGET);
+            tr.pSTR_AMT_Diff         = parseCurrency(tr.STR_AMT_Diff);
+            tr.pSTR_PER_TO_BUDGET    = parsePct(tr.STR_PER_TO_BUDGET);
 
             tr.pREGION_BUDGET_MTD    = parseCurrency(tr.REGION_BUDGET_MTD);
             tr.pREGION_SALES         = parseCurrency(tr.REGION_SALES);
             tr.pREGION_AMT_DIFF      = parseCurrency(tr.REGION_AMT_DIFF);
             tr.pREGION_PER_TO_BUDGET = parsePct(tr.REGION_PER_TO_BUDGET);
 
-            tr.pDM_TOTAL_BUDGET   = parseCurrency(tr.DM_TOTAL_BUDGET);
-            tr.pDM_TOTAL_SALES    = parseCurrency(tr.DM_TOTAL_SALES);
-            tr.pDM_TOTAL_AMT_DIFF = parseCurrency(tr.DM_TOTAL_AMT_DIFF);
-            tr.pDM_PER_TO_BUDGET  = parsePct(tr.DM_PER_TO_BUDGET);
+            tr.pDM_TOTAL_BUDGET      = parseCurrency(tr.DM_TOTAL_BUDGET);
+            tr.pDM_TOTAL_SALES       = parseCurrency(tr.DM_TOTAL_SALES);
+            tr.pDM_TOTAL_AMT_DIFF    = parseCurrency(tr.DM_TOTAL_AMT_DIFF);
+            tr.pDM_PER_TO_BUDGET     = parsePct(tr.DM_PER_TO_BUDGET);
 
             summaryHTML.push(
                 "<tr>",
-                    "<td class='text-right'>"+tr.Month+"/"+tr.Year+"</td>",
-                    "<td class='text-right'>"+tr.pSALES.parsed+"</td>",
-                    "<td class='text-right'>"+tr.pLY_SALES_MTD.parsed+"</td>",
-                    "<td class='text-right'>"+tr.pBUDGET.parsed+"</td>",
-                    "<td class='text-right'>"+tr.pTOTAL_BUDGET.parsed+"</td>",
-                    "<td class='text-right "+ ((tr.pSTR_AMT_Diff.isNegative) ? "text-danger" : "text-success") +"'>"+tr.pSTR_AMT_Diff.parsed+"</td>",
-                    "<td class='text-right "+ ((tr.pSTR_PER_TO_BUDGET.isNegative) ? "text-danger" : "text-success") +"'>"+tr.pSTR_PER_TO_BUDGET.parsedDec+"</td>",
+                    /* Month */        "<td class='text-right'>"+tr.Month+"/"+tr.Year+"</td>",
+                    /* Sales */        "<td class='text-right'>"+tr.pSALES.parsed+"</td>",
+                    /* Budget */       "<td class='text-right'>"+tr.pBUDGET.parsed+"</td>",
+                    /* Difference */   "<td class='text-right "+ ((tr.pSTR_AMT_Diff.isNegative) ? "text-danger" : "text-success") +"'>"+tr.pSTR_AMT_Diff.parsed+"</td>",
+                    /* Difference % */  "<td class='text-right "+ ((tr.pSTR_PER_TO_BUDGET.isNegative) ? "text-danger" : "text-success") +"'>"+tr.pSTR_PER_TO_BUDGET.parsedDec+"</td>",
+                    /* LY Sales MTD */ "<td class='text-right'>"+tr.pLY_SALES_MTD.parsed+"</td>",
+                    /* Total Budget */ "<td class='text-right'>"+tr.pTOTAL_BUDGET.parsed+"</td>",
                 "</tr>"
             );
 
