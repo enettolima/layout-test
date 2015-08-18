899ff6593f478e3d095f308591d4c634b68b8412
Chad C. Davis <chad.davis@gmail.com>
2015-08-18 14:14:34 -0500

    Passport 404

--- laravel/app/start/global.php
+++ laravel/app/start/global.php
@@ -84,3 +84,9 @@ App::down(function()
 */
 
 require app_path().'/filters.php';
+
+
+App::missing(function($exception)
+{
+    return Response::view('errors.missing', array(), 404);
+});
---
+++ laravel/app/views/errors/missing.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.barebones')
+
+@section('content')
+
+<h1>Error: 404 Not Found</h1>
+<h2>The Hermit Crabs took your page!</h2>
+<img src="/images/hermy404.jpg">
+<h3>Dang Hermit Crabs.</h3>
+<h5>(I think I see it there under that 8-ball shell.)</h5>
+<a href="/">Back to Safety</a>
+
+@stop
