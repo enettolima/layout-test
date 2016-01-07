abda2964e085845817328998bc657db10664b139
Chad C. Davis <chad.davis@gmail.com>
2016-01-07 15:49:18 -0600

    Super simple "get stores" controller / method (wide open, no security) implemented on Passport

--- laravel/app/controllers/SaController.php
+++ laravel/app/controllers/SaController.php
@@ -4,9 +4,29 @@ class SaController extends BaseController
 {
     public function getStores()
     {
-        $user = Request::segment(3);
-        $foo = array('chad', 'erika', 'netto', 'marcelo');
-        return Response::json($foo);
-    }
+        $response = array();
 
+        try{
+
+            $u = trim(Request::segment(3));
+
+            if (preg_match('/^(\d\d\d).*$/', $u)) {
+
+                $user = User::where('username', $u)->firstOrFail();
+
+                $stores = $user->getStores();
+
+                $response['data'] = $stores;
+            } else {
+                throw new Exception("Invalid user form.");
+            }
+
+        }catch(Exception $e){
+
+            $response['errors'][] = $e->getMessage();
+        }
+
+        return Response::json($response);
+
+    }
 }
