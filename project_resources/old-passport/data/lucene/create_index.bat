ab58143bccc8b96967178c0df51053ea62e2642b
Chad C. Davis <chad.davis@gmail.com>
2016-03-15 12:09:04 -0500

    Moving old-passport code to new repo

--- project_resources/old-passport/data/lucene/create_index.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-cd %1
-set PATH=c:\web\php/;c:\web\oracle
-set ORACLE_HOME=c:\web\oracle
-php create_index.php
--- project_resources/old-passport/data/lucene/test_index.bat
+++
@@ -1,4 +0,0 @@
-@set PATH=c:\web\php/;c:\web\oracle
-@set ORACLE_HOME=c:\web\oracle
-@php test_index.php
-@pause
