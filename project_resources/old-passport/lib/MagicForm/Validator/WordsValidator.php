9f959cbbc70166b84d661061eafb6c717c8a9030
Chad C. Davis <chad.davis@gmail.com>
2016-03-15 12:09:04 -0500

    Moving old-passport code to new repo

--- project_resources/old-passport/lib/MagicForm/Validator/WordsValidator.php
+++
@@ -1,6 +0,0 @@
-<?php
-class WordsValidator {
-	public function isValid($element) {
-		return !preg_match('/[^A-Za-z0-9\' ]/', $element->value);
-	}
-}
--- project_resources/old-passport/vendor/MagicForm/Validator/MatchValidator.php
+++
@@ -1,6 +0,0 @@
-<?php
-class MatchValidator {
-	public function isValid($element, $match) {
-		return isset($_POST[$match]) && $element->value == $_POST[$match];
-	}
-}
